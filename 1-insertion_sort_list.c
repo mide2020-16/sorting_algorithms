f34eb75d4482607abcebe69cb43abe8aee5a0ad4
mide2020-16 <efuwapeayomide51@gmail.com>
2024-03-04 09:24:09 +0100

    big O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked
+ * @list: Double pointer to the head of the linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp, *prev;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current->next;
+		prev = current->prev;
+		while (prev != NULL && prev->n > current->n)
+		{
+			/* Swap current and prev nodes */
+			if (prev->prev != NULL)
+				prev->prev->next = current;
+			if (current->next != NULL)
+				current->next->prev = prev;
+			prev->next = current->next;
+			current->prev = prev->prev;
+			prev->prev = current;
+			current->next = prev;
+
+			/* Update head if necessary */
+			if (*list == prev)
+				*list = current;
+
+			/* Print the list after swapping */
+			print_list(*list);
+
+			prev = current->prev;
+		}
+		current = temp;
+	}
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,91 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ *
+ * Description: This function swaps the values of two integers.
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * partition - Partitions the array using the last element as pivot.
+ * @array: Pointer to the array to be partitioned.
+ * @low: Index of the first element of the array.
+ * @high: Index of the last element of the array.
+ *
+ * Description: This function partitions the array into two sub-arrays
+ * such that elements smaller than the pivot are placed
+ * to the left, and elements greater than or equal to
+ * the pivot are placed to the right.
+ *
+ * Return: The index of the pivot element after partitioning.
+ */
+int partition(int *array, int low, int high)
+{
+	int pivot, i, j;
+
+	pivot = array[high];
+	i = low - 1;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+			print_array(array, high + 1);
+		}
+	}
+
+	swap(&array[i + 1], &array[high]);
+	return (i + 1);
+}
+
+/**
+ * quick_sort_helper - Recursively sorts the array using Quick Sort algorithm.
+ * @array: Pointer to the array to be sorted.
+ * @low: Index of the first element of the array.
+ * @high: Index of the last element of the array.
+ *
+ * Description: This function recursively applies Quick Sort algorithm
+ * to sort the elements of the array in ascending order.
+ */
+void quick_sort_helper(int *array, int low, int high)
+{
+	int pi;
+
+	if (low < high)
+	{
+		pi = partition(array, low, high);
+
+		quick_sort_helper(array, low, pi - 1);
+		quick_sort_helper(array, pi + 1, high);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick Sort.
+ *
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ *
+ * Description: This function sorts the elements of the array in ascending order
+ * using the Quick Sort algorithm with Lomuto partition scheme.
+ * It prints the array after each swap operation.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_helper(array, 0, size - 1);
+}

9d560408aefeff55d3cc14c3c66d680b1a93e459
mide2020-16 <efuwapeayomide51@gmail.com>
2024-03-04 09:43:37 +0100

    big O

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,35 +9,35 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
-    int temp;
+	size_t i, j, min_index;
+	int temp;
 
-    /* Check if the array is NULL or has less than 2 elements */
-    if (array == NULL || size < 2)
-        return;
+	/* Check if the array is NULL or has less than 2 elements */
+	if (array == NULL || size < 2)
+		return;
 
-    /* Iterate over the array to select the smallest element */
-    for (i = 0; i < size - 1; i++)
-    {
-        /* Assume the current index has the smallest element */
-        min_index = i;
+	/* Iterate over the array to select the smallest element */
+	for (i = 0; i < size - 1; i++)
+	{
+		/* Assume the current index has the smallest element */
+		min_index = i;
 
-        /* Find the index of the smallest element in the unsorted part */
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+		/* Find the index of the smallest element in the unsorted part */
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
 
-        /* Swap the smallest element with the current index, if necessary */
-        if (min_index != i)
-        {
-            temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
+		/* Swap the smallest element with the current index, if necessary */
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
 
-            /* Print the array after each swap */
-            print_array(array, size);
-        }
-    }
+			/* Print the array after each swap */
+			print_array(array, size);
+		}
+	}
 }
